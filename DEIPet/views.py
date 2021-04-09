aaa51a34e714d777fc5f3f6aec36de7ae4392eed
Rafael Oliveira <rafdev.x@gmail.com>
2021-04-09 21:34:18 +0100

    ugh, can't have sorting or search

--- DEIPet/views.py
+++ DEIPet/views.py
@@ -6,17 +6,11 @@ from django.shortcuts import render
 from . import petstore
 
 
-def list_pets(
-    request: HttpRequest, page=1, page_size=30, sort_by="id", sort_dir="asc"
-) -> HttpResponse:
-    """Show a page that displays a list of pets in a user-intuitive manner."""
-
-    possible_sorts, possible_dirs = ("id", "name"), ("asc", "dsc")
+def list_pets(request: HttpRequest, page=1, page_size=30) -> HttpResponse:
+    """Show a page that displays a list of pets with pagination."""
 
     page = page if page >= 1 else 1
     page_size = page_size if 1 <= page_size <= 50 else 30
-    sort_by = sort_by if sort_by in possible_sorts else possible_sorts[0]
-    sort_dir = sort_dir if sort_dir in possible_dirs else possible_dirs[0]
 
     # get an extra item to see if next page exists
     pets = petstore.get_pets_page(page, page_size + 1)
@@ -28,12 +22,10 @@ def list_pets(
         request,
         "DEIPet/index.html",
         {
-            "pets": sorted(pets, key=lambda k: k[sort_by], reverse=sort_dir == "dsc"),
+            "pets": pets,
             "page": page,
             "prev_page_exists": prev_page_exists,
             "next_page_exists": next_page_exists,
-            "sort_by": sort_by,
-            "sort_dir": sort_dir,
         },
     )
 
--- README.md
+++ README.md
@@ -27,6 +27,8 @@ _Não está apto para produção, por motivos evidentes._
 
 -   Ao contrário do que é usual, a API Petstore disponibilizada não expõe nenhuma forma de determinar _quantos_ animais de estimação existem no total. Assim, implementando paginação na lista de animais, a única maneira de mostrar ao utilizador quantas páginas há ao todo a seria obter todos e contá-los ­— o que claramente é contra o propósito da própria Petstore fazer paginação e não seria escalável. Assim, foi tomada a decisão de mostrar apenas botões de navegação, _sem_ a informação da quantidade total.
 
+-   Pela mesma razão, não foi implementada uma funcionalidade de pesquisa nem de ordenação, pois tais operações apenas poderiam ser feitas por página em vez de no geral, o que seria pouco útil (ou até enganador) para o utilizador.
+
 ---
 
 <b id="fn1"><sup>1</sup></b> Visto esta ser a única aplicação, para efeitos de conveniência, foi tomada a liberdade de criar um _redirect_, pelo que basta aceder ao endereço [http://127.0.0.1:8000]() sem qualquer _path_. [↩](#a1)
